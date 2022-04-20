04a396ef0760a95498bcf7b753bc9ba6b0ea948e
Emeka Nwadiogbu, Jr <emekanwadiogbu.jr@me.com>
2022-04-20 01:02:17 -0700

    completed portfolio components and css

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,58 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://dribble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Blockchain mobile app design',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18061452-Blockchain-mobile-app-design'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Financial landing page',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16491476-Financial-landing-page'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Home Page Experience',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18053124-Home-Page-Experience'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: '#121 - Concept shots',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18058317--121-Concept-shots'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: '#105 - Concept shots',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16097897--105-Concept-shots'
+  }
+
+]
 
 const Portfolio = () => {
   return (
@@ -8,12 +61,23 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="div portfolio__item-image"></div>
-            <h3>This is a portfolio item title</h3>
-            <a href='https://github.com' className='btn'>GitHub</a>
-            <a href='https://dribbble.com/alien_pixels' className='btn btn-primary'>Live Demo</a>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+          <div className="div portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className='btn' target='_blank'>GitHub</a>
+              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+            </div>
         </article>
+            )
+          })
+        }
+      
       </div>
     </section>
   )
