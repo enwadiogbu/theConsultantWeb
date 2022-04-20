21dc125eee6026a3286e2892b05c8845530d880f
Emeka Nwadiogbu, Jr <emekanwadiogbu.jr@me.com>
2022-04-19 23:56:20 -0700

    finished experience and services components

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="div portfolio__item-image"></div>
+            <h3>This is a portfolio item title</h3>
+            <a href='https://github.com' className='btn'>GitHub</a>
+            <a href='https://dribbble.com/alien_pixels' className='btn btn-primary'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
